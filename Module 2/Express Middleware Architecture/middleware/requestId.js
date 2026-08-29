4be68541f9b161fd8dea6df6be7c721265cc1e69
spandanax <spanduirl@gamil.com>
2026-08-29 12:56:40 +0530

    Solution for LU2.4

--- Module 2/Express Middleware Architecture/middleware/requestId.js
+++ Module 2/Express Middleware Architecture/middleware/requestId.js
@@ -12,4 +12,7 @@ const { randomUUID } = require('crypto');
 
 module.exports = function requestId(req, res, next) {
   // TODO: implement the four steps described above.
+  req.id = randomUUID();
+  res.setHeader('X-Request-Id', req.id);
+  next(); 
 };
